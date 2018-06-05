645db561ee3f279ba20b91f6d018b43223e58ccf
Raymond Guo <guor.lei@berkeley.edu>
2018-06-05 17:04:13 -0400

    Add project page with image grid and basic overlaying

---
+++ scripts/project_script.js
@@ -0,0 +1,26 @@
+$(".menu_item").hover(
+	function() {
+		$(this).fadeOut(100, function() {
+			$(this).css('background-color', 'black');
+			$(this).css('color', 'white');
+			$(this).fadeTo(100, 1);
+		});
+	},
+	function() {
+		$(this).fadeOut(100, function() {
+			$(this).css('background-color', 'white');
+			$(this).css('color', 'black');
+			$(this).fadeTo(100, 1);
+		});
+});
+
+$(function () {
+	var $win = $(window);
+	$win.scroll(function () {
+    if ($win.scrollTop() <= 100) {
+      $("#navbar").hide(500);
+    } else {
+      $("#navbar").show(500);
+    }
+  });
+});
