5d6033e74b1267e2b9b0ed27d7ce736f931766a9
Raymond Guo <guor.lei@berkeley.edu>
2018-06-05 16:02:33 -0400

    Add teaching page

---
+++ scripts/teaching_script.js
@@ -0,0 +1,34 @@
+$(".link").hover(
+	function() {
+		$(this).children().fadeTo(150, 0.5);
+	},
+	function() {
+		$(this).children().fadeTo(150, 1);
+	});
+
+$(".menu_item").hover(
+	function() {
+		$(this).fadeOut(100, function() {
+			$(this).css('background-color', 'black');
+			$(this).css('color', 'white');
+			$(this).fadeTo(100, 1);
+		});
+	},
+	function() {
+		$(this).fadeOut(100, function() {
+			$(this).css('background-color', 'white');
+			$(this).css('color', 'black');
+			$(this).fadeTo(100, 1);
+		});
+});
+
+$(function () {
+	var $win = $(window);
+	$win.scroll(function () {
+    if ($win.scrollTop() <= 100) {
+      $("#navbar").hide(500);
+    } else {
+      $("#navbar").show(500);
+    }
+  });
+});
