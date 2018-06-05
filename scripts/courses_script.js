c597882f92169b6506483f696a064d17c6c47ff1
Raymond Guo <guor.lei@berkeley.edu>
2018-06-05 11:01:38 -0400

    Finish courses page completely

--- scripts/courses_script.js
+++ scripts/courses_script.js
@@ -1,31 +1,34 @@
 $(".link").hover(
-	function() {$(this).fadeTo(300, 0.5);},
-	function() {$(this).fadeTo(300, 1);});
+	function() {
+		$(this).children().fadeTo(150, 0.5);
+	},
+	function() {
+		$(this).children().fadeTo(150, 1);
+	});
 
 $(".menu_item").hover(
 	function() {
-		$(this).fadeOut(100, function() {
+		$(this).fadeOut(150, function() {
 			$(this).css('background-color', 'black');
 			$(this).css('color', 'white');
-			$(this).fadeTo(100, 1);
+			$(this).fadeTo(150, 1);
 		});
 	},
 	function() {
-		$(this).fadeOut(100, function() {
+		$(this).fadeOut(150, function() {
 			$(this).css('background-color', 'white');
 			$(this).css('color', 'black');
-			$(this).fadeTo(100, 1);
+			$(this).fadeTo(150, 1);
 		});
 });
 
-$(function() {
-	$("#navbar").hover(
-		function () {
-				$("#navbar").fadeTo('fast',1);
-			},
-			function () {
-				if (document.documentElement.scrollTop!=0) {
-					$("#navbar").fadeTo('fast',0);}
-				}
-			);
+$(function () {
+	var $win = $(window);
+	$win.scroll(function () {
+    if ($win.scrollTop() <= 100) {
+      $("#navbar").hide(500);
+    } else {
+      $("#navbar").show(500);
+    }
+  });
 });
